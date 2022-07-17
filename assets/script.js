4961d6b4f5cfeca81a378f0d915ba330962eb554
mulloa90 <marioulloa@Marios-iMac.attlocal.net>
2022-07-17 14:14:37 -0700

    updated text area

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,63 @@
 var dt = new Date();
+var currentHour = dt.getHours();
 document.getElementById("date-time").innerHTML = dt;
 var mainContainer = document.getElementById("main-container");
 
-var hours = ["08", "09", "10", "11", "12", "01", "02", "03", "04", "05"];
+var hours = ["08", "09", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 hours.forEach(function (hour) {
   //create the html block
   var newRow = document.createElement("div");
   newRow.classList.add("row", "time-block");
-  newRow.innerHTML = `<div class="hour col-md-1">${hour}:00</div>
-    <textarea class="description col-md-10" name="2" id="8" placeholder="Schedule task"></textarea>
-    <button class="savebtn col-md-1" onclick="saveText(8)">Save</button>`;
+  newRow.innerHTML = `<div class="hour col-md-1">${getHourText(hour)}</div>
+    <textarea class="description col-md-10 ${getTextAreaClass(
+      hour
+    )}" id='hour-${hour}' placeholder="Schedule task"></textarea>
+    <button data-hour=${hour} class="savebtn col-md-1" onclick="saveText(this)">Save</button>`;
   mainContainer.append(newRow);
   //append it to the container
 });
+
+function saveText(button) {
+  //   console.log();
+  var textArea = document.getElementById(`hour-${button.dataset.hour}`);
+  console.log(textArea.value);
+}
+
+function getTextAreaClass(hour) {
+  if (hour < currentHour) {
+    return "past";
+  } else if (hour == currentHour) {
+    return "present";
+  } else if (hour > currentHour) {
+    return "future";
+  }
+}
+
+function getHourText(hour) {
+  switch (hour) {
+    case "08":
+      return "08:00am";
+    case "09":
+      return "09:00am";
+    case "10":
+      return "10:00am";
+    case "11":
+      return "11:00am";
+    case "12":
+      return "12:00pm";
+    case "13":
+      return "01:00pm";
+    case "14":
+      return "02:00pm";
+    case "15":
+      return "03:00pm";
+    case "16":
+      return "04:00pm";
+    case "17":
+      return "05:00pm";
+  }
+}
 
 /*WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past, present, or future
