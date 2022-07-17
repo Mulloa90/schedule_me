d145552b27df6b619f5da8e938f59bb0bee46926
mulloa90 <marioulloa@Marios-iMac.attlocal.net>
2022-07-17 00:55:08 -0700

    updated css

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,11 @@
-var todayDate = moment().format(dddd, MMM, yyyy);
-$("#currentDay").html(todayDate);
+var dt = new Date();
+document.getElementById('date-time').innerHTML=dt;
+
+/*WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist*/
